0c855239e500a42bb422f628606b32086c2cd73c
Aravind Suri <aravind.suri@gmail.com>
2025-04-26 18:07:53 +0200

    updated the drawer layout

--- mobile/app/(drawer)/_layout.tsx
+++ mobile/app/(drawer)/_layout.tsx
@@ -3,7 +3,14 @@ import { Tabs } from 'expo-router';
 import CustomDrawerContent from '../(drawer)/CustomDrawerContent';
 
 export default function DrawerLayout() {
-  return <Drawer drawerContent={props => <CustomDrawerContent {...props} />} />;
+  return (
+    <Drawer
+      drawerContent={props => <CustomDrawerContent {...props} />}
+      screenOptions={{
+        headerTitle: '',
+      }}
+    />
+  );
 }
 
 export function TabLayout() {
